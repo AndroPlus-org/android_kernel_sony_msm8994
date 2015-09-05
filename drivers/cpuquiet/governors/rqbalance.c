a48b7f0658dd4671b0cd32674abebbad5b45f2f2
WedyDQ10 <hosigumayuugi@gmail.com>
2015-09-05 22:35:46 +0900

    Revert "cpuquiet: rqbalance: Initial HMP support"
    This reverts commit 26498a1b3231bcb88c458feae21d9d35f00658b6.

--- drivers/cpuquiet/governors/rqbalance.c
+++ drivers/cpuquiet/governors/rqbalance.c
@@ -51,13 +51,8 @@
 #define UPCORE_DELAY_MS		((RQ_SAMPLE_TIME_NS / NSEC_PER_MSEC) / 2)
 
 typedef enum {
-	/* Brings one more CPU core on-line */
 	CPU_UPCORE,
-
-	/* Keeps CPU core composition unchanged */
 	CPU_FAIR,
-
-	/* Offline one CPU core */
 	CPU_DNCORE,
 } CPU_SPEED_BALANCE;
 
@@ -69,10 +64,6 @@ typedef enum {
 
 #define USERSPACE_RUNNING	0
 #define USERSPACE_LOW_POWER	1
-
-#define CLUSTER_LITTLE		0
-#define CLUSTER_BIG		1
-#define MAX_CLUSTERS		2
 
 struct idle_info {
 	u64 idle_last;
@@ -86,14 +77,11 @@ static DEFINE_PER_CPU(unsigned int, cpu_load);
 
 static struct timer_list load_timer;
 static bool load_timer_active;
-static bool clusterswitch_mode; /* TODO: Not implemented */
-static bool soc_is_hmp;
-static unsigned int available_clusters;
 
 /* configurable parameters */
 static unsigned int  balance_level = 60;
-static unsigned int  idle_bottom_freq[MAX_CLUSTERS];
-static unsigned int  idle_top_freq[MAX_CLUSTERS];
+static unsigned int  idle_bottom_freq;
+static unsigned int  idle_top_freq;
 static unsigned long up_delay;
 static unsigned long down_delay;
 static unsigned long last_change_time;
@@ -181,9 +169,6 @@ static unsigned int cpu_highest_speed(void)
 	unsigned int maxload = 0;
 	int i;
 
-	//if (hiperf_cluster)
-	//	i = hiperf_first_cpu;
-
 	for_each_online_cpu(i) {
 		unsigned int *load = &per_cpu(cpu_load, i);
 
@@ -208,81 +193,21 @@ static unsigned int count_slow_cpus(unsigned int limit)
 	return cnt;
 }
 
-static unsigned int num_online_cluster_cpus(bool cluster)
-{
-	unsigned int cnt = 0;
-	int i;
-
-	for_each_online_cpu(i) {
-		if (topology_physical_package_id(i) == cluster)
-			cnt++;
-	}
-
-	return cnt;
-}
-
-static inline int get_offline_core(int cluster)
-{
-	int i;
-
-	for_each_possible_cpu(i) {
-		if (topology_physical_package_id(i) != cluster)
-			continue;
-
-		if (cpu_is_offline(i))
-			return i;
-	};
-
-	return 0;
-}
-
 /*
  * There are two ladders here for hysteresis. We do not want to offline
  * and online a CPU repeatedly. Thus, the down thresholds are *lower*
  * than the corresponding up threshold.
- * The size of both arrays shall be the same.
- *
- * NOTE: On HMP multi-cluster systems, the last value is used to enable
- *       onlining of the high performance cluster CPUs, but ONLY when
- *       clusterswitch (one cluster at a time) mode is enabled.
+ * NOTE: size of both arrays shall be the same.
  */
 static unsigned int nr_run_thresholds[] = {
 /*      1,	2,	3,	4 (target on-line num of cpus) */
-	250,	400,	500,	520,
-
-/*	5,	6,	7,	8 (HMP target on-line num of CPUs) */
-	550,	640,	750,	UINT_MAX
+	250,	400,	500,	UINT_MAX
 };
 
 static unsigned int nr_down_run_thresholds[] = {
 /*	1,	2,	3,	4 (target off-line num of cpus) */
-	0,	110,	290,	360,
-
-/*	5,	6,	7,	8 (HMP target off-line num of CPUs) */
-	440,	500,	550,	680
+	0,	110,	290,	360
 };
-
-/*
- * Those are hysteresis values for the high performance CPUs.
- *
- * The logic in this is to add the hysteresis value as it is if we want
- * to calculate our ladders for onlining a core (upcore), but divide by
- * half (each corresponding value) if we want to calculate the ladders
- * for offlining a core (dncore).
- *
- * This logic pretends we want less power consumption at a little
- * performance cost.
- * On mobile devices with passive cooling, this is expected to increase
- * CPU clusters efficiency by reducing heat and subsequently throttling.
- *
- * NOTE: This is used to compose *complete* run thresholds arrays for
- *       the high performance CPUs cluster when clusterswitch mode
- *       is disabled and when we can use both clusters simultaneously.
- */
-//static unsigned int hiperf_cpu_hysteresis[] = {
-/*	1,	2,	3,	4 (used only for downcore hysteresis) */
-//	15,     25,	30,	60
-//};
 
 struct runqueue_sample {
 	int64_t sample_time;
@@ -422,14 +347,11 @@ static CPU_SPEED_BALANCE balanced_speed_balance(void)
 	unsigned int max_cpus = cpuquiet_get_cpus(true);
 	unsigned int avg_nr_run = get_nr_run_avg();
 	unsigned int nr_run;
-	bool done;
 
 	/* First use the up thresholds to see if we need to bring CPUs online. */
 	pr_debug("%s: Current core count max runqueue: %d\n", __func__,
 		 nr_run_thresholds[nr_cpus - 1]);
-	for (nr_run = nr_cpus;
-		nr_run < ARRAY_SIZE(nr_run_thresholds);
-		++nr_run) {
+	for (nr_run = nr_cpus; nr_run < ARRAY_SIZE(nr_run_thresholds); ++nr_run) {
 		if (avg_nr_run <= nr_run_thresholds[nr_run - 1])
 			break;
 	}
@@ -443,13 +365,12 @@ static CPU_SPEED_BALANCE balanced_speed_balance(void)
 	 * down threshold comparison loop.
 	 */
 	for ( ; nr_run > 1; --nr_run) {
-		if (done || avg_nr_run >= nr_down_run_thresholds[nr_run - 1]) {
+		if (avg_nr_run >= nr_down_run_thresholds[nr_run - 1]) {
 			/* We have fewer things running than our down threshold.
 			   Use one less CPU. */
 			break;
 		}
 	}
-
 
 	/* Consider when to take a CPU offline.  We need to have at least
 	 * one extra idle CPU, or either of: */
@@ -475,69 +396,6 @@ static CPU_SPEED_BALANCE balanced_speed_balance(void)
 
 	/* Otherwise, we have to online one more CPU */
 	return CPU_UPCORE;
-}
-
-static void hotplug_smp_final_decision(unsigned int cpu, bool up)
-{
-	if (up)
-		/* TODO: If we just put another CPU online, reduce the frequency of
-		 * the CPU since we have increased the overall computational
-		 * power of the processor. */
-		cpuquiet_wake_cpu(cpu, false);
-	else
-		cpuquiet_quiesence_cpu(cpu, false);
-}
-
-static void hotplug_hmp_final_decision(unsigned int cpu, bool up)
-{
-	unsigned int cluster = topology_physical_package_id(cpu);
-	unsigned int little_cores, big_cores;
-	unsigned int little_max = 4; /* TODO: Get max (SoC) little cores properly */
-
-	if (up) {
-		/* Analyze online cores situation */
-		big_cores = num_online_cluster_cpus(CLUSTER_BIG);
-		little_cores = num_online_cluster_cpus(CLUSTER_LITTLE);
-
-pr_info("----_RQBALANCE: BIG_CORES = %u LITTLE_CORES= %u\n", big_cores, little_cores);
-
-#if 1
-		/* If we want 4 little cores, we can instead use one
-		   big core and two little */
-		if (cluster == CLUSTER_LITTLE &&
-		    little_cores >= little_max - 1 &&
-		    big_cores == 0) {
-			cpuquiet_wake_cpu(
-				get_offline_core(CLUSTER_BIG), false);
-			cpuquiet_quiesence_cpu(
-				get_offline_core(CLUSTER_LITTLE), false);
-			return;
-		};
-#else
-		if (cluster == CLUSTER_LITTLE) {
-			if (little_cores >= little_max - 1 &&
-			    big_cores == 0) {
-				cpuquiet_wake_cpu(
-					get_offline_core(CLUSTER_BIG),
-							 false);
-				cpuquiet_quiesence_cpu(
-					get_offline_core(CLUSTER_LITTLE),
-							 false);
-				return;
-			}
-
-			if (little_cores == little_max - 1) {
-				cpuquiet_wake_cpu(
-					get_offline_core(CLUSTER_LITTLE),
-							false);
-				return;
-			}
-		};
-#endif
-		cpuquiet_wake_cpu(cpu, false);
-	} else {
-		cpuquiet_quiesence_cpu(cpu, false);
-	}
 }
 
 /*
@@ -616,10 +474,13 @@ static void rqbalance_work_func(struct work_struct *work)
 
 	if (cpu < nr_cpu_ids) {
 		last_change_time = now;
-		if (!soc_is_hmp)
-			hotplug_smp_final_decision(cpu, up);
+		if (up)
+			/* TODO: If we just put another CPU online, reduce the frequency of
+			 * the CPU since we have increased the overall computational
+			 * power of the processor. */
+			cpuquiet_wake_cpu(cpu, false);
 		else
-			hotplug_hmp_final_decision(cpu, up);
+			cpuquiet_quiesence_cpu(cpu, false);
 	}
 }
 
@@ -628,27 +489,18 @@ static int balanced_cpufreq_transition(struct notifier_block *nb,
 {
 	struct cpufreq_freqs *freqs = data;
 	unsigned long cpu_freq;
-	int n;
-
-	/*
-	 * If we have at least one BIG CPU online, then take
-	 * a decision based on calculations made on BIG.
-	 */
-	if (soc_is_hmp)
-		if (num_online_cluster_cpus(CLUSTER_BIG) > 0)
-			n = 1;
 
 	if (state == CPUFREQ_POSTCHANGE || state == CPUFREQ_RESUMECHANGE) {
 		cpu_freq = freqs->new;
 
 		switch (rqbalance_state) {
 		case IDLE:
-			if (cpu_freq >= idle_top_freq[n]) {
+			if (cpu_freq >= idle_top_freq) {
 				rqbalance_state = UP;
 				queue_delayed_work(
 					rqbalance_wq, &rqbalance_work, up_delay);
 				start_load_timer();
-			} else if (cpu_freq <= idle_bottom_freq[n]) {
+			} else if (cpu_freq <= idle_bottom_freq) {
 				rqbalance_state = DOWN;
 				queue_delayed_work(
 					rqbalance_wq, &rqbalance_work,
@@ -657,7 +509,7 @@ static int balanced_cpufreq_transition(struct notifier_block *nb,
 			}
 			break;
 		case DOWN:
-			if (cpu_freq >= idle_top_freq[n]) {
+			if (cpu_freq >= idle_top_freq) {
 				rqbalance_state = UP;
 				queue_delayed_work(
 					rqbalance_wq, &rqbalance_work, up_delay);
@@ -665,7 +517,7 @@ static int balanced_cpufreq_transition(struct notifier_block *nb,
 			}
 			break;
 		case UP:
-			if (cpu_freq <= idle_bottom_freq[n]) {
+			if (cpu_freq <= idle_bottom_freq) {
 				rqbalance_state = DOWN;
 				queue_delayed_work(rqbalance_wq,
 					&rqbalance_work, up_delay);
@@ -747,8 +599,8 @@ ssize_t show_thresholds(struct cpuquiet_attribute *cattr,
 }
 
 CPQ_BASIC_ATTRIBUTE(balance_level, 0644, uint);
-//CPQ_BASIC_ATTRIBUTE(idle_bottom_freq[0], 0644, uint);
-//CPQ_BASIC_ATTRIBUTE(idle_top_freq[0], 0644, uint);
+CPQ_BASIC_ATTRIBUTE(idle_bottom_freq, 0644, uint);
+CPQ_BASIC_ATTRIBUTE(idle_top_freq, 0644, uint);
 CPQ_BASIC_ATTRIBUTE(load_sample_rate, 0644, uint);
 CPQ_BASIC_ATTRIBUTE(userspace_suspend_state, 0644, uint);
 CPQ_ATTRIBUTE(up_delay, 0644, ulong, delay_callback);
@@ -761,8 +613,8 @@ CPQ_ATTRIBUTE_CUSTOM(nr_run_thresholds, 0644,
 
 static struct attribute *rqbalance_attributes[] = {
 	&balance_level_attr.attr,
-//	&idle_bottom_freq_attr.attr,
-//	&idle_top_freq_attr.attr,
+	&idle_bottom_freq_attr.attr,
+	&idle_top_freq_attr.attr,
 	&userspace_suspend_state_attr.attr,
 	&up_delay_attr.attr,
 	&down_delay_attr.attr,
@@ -834,54 +686,6 @@ int rqbalance_pm_notify(struct notifier_block *notify_block,
 	return NOTIFY_OK;
 }
 
-/*
- * Retrieves frequency informations from CPUFreq.
- * This is needed for RQBalance's hotplugging decider
- * algorithms.
- *
- * \return Returns success (0) or negative errno.
- */
-static int rqbalance_get_package_info(void)
-{
-	struct cpufreq_frequency_table *table;
-	int count, i, prev_cluster, cur_cluster;
-
-	for_each_possible_cpu(i) {
-		cur_cluster = topology_physical_package_id(i);
-		if (cur_cluster == prev_cluster)
-			continue;
-
-		prev_cluster = cur_cluster;
-		available_clusters++;
-
-		/*
-		 * Get CPUFreq frequency table. RQBALANCE only works with
-		 * tables composed of at least 4 frequency entries.
-		 * This requirement has to be fullfilled for ALL clusters.
-		 */
-		table = cpufreq_frequency_get_table(i);
-		if (!table)
-			return -EINVAL;
-
-		for (count = 0;
-		     table[count].frequency != CPUFREQ_TABLE_END;
-		     count++);
-
-		if (count < 4)
-			return -EINVAL;
-
-		idle_top_freq[cur_cluster] =
-				table[(count / 2) - 1].frequency;
-		idle_bottom_freq[cur_cluster] =
-				table[(count / 2) - 2].frequency;
-	};
-
-	/* TODO: Check if we are effectively using HMP!!! This is NOT OK!!! */
-	soc_is_hmp = (available_clusters > 1) ? true : false;
-
-	return 0;
-}
-
 static void rqbalance_stop(void)
 {
 	/*
@@ -907,7 +711,8 @@ static void rqbalance_stop(void)
 
 static int rqbalance_start(void)
 {
-	int err, i, max_sys_cpus = 0;
+	int err, count;
+	struct cpufreq_frequency_table *table;
 
 	err = rqbalance_sysfs();
 	if (err)
@@ -923,15 +728,17 @@ static int rqbalance_start(void)
 	up_delay = msecs_to_jiffies(UPCORE_DELAY_MS);
 	down_delay = msecs_to_jiffies(DNCORE_DELAY_MS);
 
-	err = rqbalance_get_package_info();
-	if (err)
-		return err;
+	table = cpufreq_frequency_get_table(0);
+	if (!table)
+		return -EINVAL;
 
-	for_each_possible_cpu(i)
-		max_sys_cpus++;
+	for (count = 0; table[count].frequency != CPUFREQ_TABLE_END; count++);
 
-	/* Set value as target MAX on-line number of CPUs */
-	nr_run_thresholds[max_sys_cpus - 1] = UINT_MAX;
+	if (count < 4)
+		return -EINVAL;
+
+	idle_top_freq = table[(count / 2) - 1].frequency;
+	idle_bottom_freq = table[(count / 2) - 2].frequency;
 
 	cpufreq_register_notifier(&balanced_cpufreq_nb,
 		CPUFREQ_TRANSITION_NOTIFIER);
